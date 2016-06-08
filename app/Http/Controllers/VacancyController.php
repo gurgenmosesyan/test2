7742f3a08beef9b69cb67ba102e6223039ce6d52
gurgenmosesyan <gurgen.mosesyan@gmail.com>
2016-06-09 01:50:27 +0400

    commit

--- app/Http/Controllers/VacancyController.php
+++ app/Http/Controllers/VacancyController.php
@@ -3,24 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Models\Vacancy\Vacancy;
+use App\Models\Background\Background;
 
 class VacancyController extends Controller
 {
     public function all()
     {
+        $background = Background::first();
         $vacancies = Vacancy::select('vacancies.id','vacancies.published_at','ml.title','ml.function')->joinMl()->latest()->get();
 
         return view('vacancy.all')->with([
+            'background' => $background->getImage('homepage'),
             'vacancies' => $vacancies
         ]);
     }
 
     public function index($lngCode, $id)
     {
+        $background = Background::first();
         $vacancy = Vacancy::select('ml.*','vacancies.asap','vacancies.published_at','vacancies.start_date','vacancies.open_date','vacancies.deadline')
             ->joinMl()->where('vacancies.id', $id)->firstOrFail();
 
         return view('vacancy.index')->with([
+            'background' => $background->getImage('homepage'),
             'vacancy' => $vacancy
         ]);
     }
--- public/js/mobile.js
+++ public/js/mobile.js
@@ -1,123 +1,65 @@
 'use strict';
 
 var $mobile = {};
-$mobile.mobileMode = false;
 
-$mobile.initNav = function() {
-    var mobileNav = '<a href="#" id="mobile-nav" class="db">'+
-                        '<span class="db"></span>'+
-                        '<span class="db"></span>'+
-                        '<span class="db"></span>'+
+$mobile.initBurger = function() {
+    var mobileMenu = $('#nav');
+    mobileMenu.appendTo('#header');
+
+    var burger = '<a href="#" id="burger" class="db">'+
+                        '<span class="burger1 db"></span>'+
+                        '<span class="burger2 db"></span>'+
+                        '<span class="burger3 db"></span>'+
                     '</a>';
-    mobileNav = $(mobileNav);
+    burger = $(burger);
 
-    var mobileMenu = '<ul id="mobile-menu" class="dpn">'+
-                        '<li><a href="'+ $main.basePath('/') +'">'+ $trans.get('www.menu.about') +'</a></li>'+
-                        '<li><a href="'+ $main.basePath('/products') +'">'+ $trans.get('www.menu.products') +'</a></li>'+
-                        '<li><a href="'+ $main.basePath('/partners') +'">'+ $trans.get('www.menu.partners') +'</a></li>'+
-                        '<li class="last"><a href="'+ $main.basePath('/contact') +'">'+ $trans.get('www.menu.contact') +'</a></li>'+
-                     '</ul>';
-    mobileMenu = $(mobileMenu);
-
-    $('#header').append(mobileNav).append(mobileMenu);
-
-    mobileNav.on('click', function() {
+    burger.on('click', function() {
         $(this).toggleClass('open');
-        if (mobileMenu.is(':visible')) {
-            mobileMenu.slideUp(300);
-        } else {
-            mobileMenu.slideDown(300);
-        }
+        mobileMenu.stop().slideToggle();
         return false;
     });
+    $('#header').prepend(burger);
 };
 
-$mobile.initFooter = function() {
-    $('#footer-main').appendTo($('#footer-inner'));
-};
-
-$mobile.initSlider = function() {
-    function ScaleSlider() {
-        var refSize = $main.slider.$Elmt.parentNode.clientWidth;
-        if (refSize) {
-            refSize = Math.min(refSize, 1200);
-            $main.slider.$ScaleWidth(refSize);
-        }
-        else {
-            window.setTimeout(ScaleSlider, 30);
-        }
-    }
-    ScaleSlider();
-    $(window).bind("load", ScaleSlider);
-    $(window).bind("resize", ScaleSlider);
-    $(window).bind("orientationchange", ScaleSlider);
-};
-
-$mobile.initCategories = function(categoriesBox) {
-    var pathname = document.location.pathname;
-    if (pathname != '/products') {
-        $('html, body').animate({
-            scrollTop: categoriesBox.offset().top - 420
-        }, 500);
-    }
-    var leftArrow = $('<a href="#" class="cat-arrow cat-left"></a>'),
-        rightArrow = $('<a href="#" class="cat-arrow cat-right"></a>');
-    leftArrow.on('click', function() {
-        var cLink = categoriesBox.find('a.active'),
-            prevLink = cLink.parent('li').prev('li').find('a');
-        if (prevLink.length == 0) {
-            return false;
-        }
-        document.location.href = $main.basePath('/products/'+prevLink.data('alias'));
-        return false;
-    });
-    rightArrow.on('click', function() {
-        var cLink = categoriesBox.find('a.active'),
-            nextLink = cLink.parent('li').next('li').find('a');
-        if (nextLink.length == 0) {
-            return false;
-        }
-        document.location.href = $main.basePath('/products/'+nextLink.data('alias'));
-        return false;
-    });
-    categoriesBox.append(leftArrow).append(rightArrow);
-};
-
-$mobile.initPartner = function() {
-    $('#partners .partner:odd').addClass('odd');
-    $('#partners .partner-box').on('click', function() {
+$mobile.lngSwitcher = function() {
+    var languages = $('#lng-switcher');
+    var html =  '<div id="m-lng-switcher">'+
+                    '<div class="top-switcher">'+ $('.active a', languages).text() +'</div>'+
+                    '<div class="bottom-switcher dpn">';
+    $('li', languages).each(function() {
         var self = $(this);
-        var title = self.find('.title').text(),
-            text = self.find('.sub-title').text(),
-            link, html;
-        link = self.attr('href') ? '<p class="link"><a href="'+self.attr('href')+'" target="_blank">Link</a></p>' : '';
-        html =  '<div id="partner-overlay">'+
-                    '<div class="partner-mobile">'+
-                        '<a href="#" class="close"></a>'+
-                        '<div class="partner-mobile-info">'+
-                            '<h3>'+title+'</h3>'+
-                            '<p>'+text+'</p>'+
-                            link+
-                        '</div>'+
-                    '</div>'+
-                '</div>';
-        html = $(html);
-        html.find('.close').on('click', function() {
-            $('#header, #content, #footer').removeClass('filter-alpha');
-            html.remove();
-            return false;
-        });
-        $('#header, #content, #footer').addClass('filter-alpha');
-        $('body').append(html);
-        return false;
+        if (!self.hasClass('active') && !self.hasClass('cb')) {
+            var link = self.find('a');
+            html += '<a href="'+ link.attr('href') +'">'+ link.text() +'</a>';
+        }
     });
+    html += '</div></div>';
+    html = $(html);
+    $('.top-switcher', html).on('click', function() {
+        if ($('.bottom-switcher', html).is(':visible')) {
+            $('.bottom-switcher', html).stop().slideUp();
+            $(document).unbind('click');
+        } else {
+            $('.bottom-switcher', html).stop().slideDown();
+            $(document).bind('click', function(e) {
+                var clicked = $(e.target);
+                if (!clicked.parents().is('#m-lng-switcher')) {
+                    $('.bottom-switcher', html).slideToggle();
+                    $(document).unbind('click');
+                }
+            });
+        }
+    });
+    $('#header').prepend(html);
 };
 
 $mobile.init = function() {
+    $mobile.initBurger();
 
-    //$('#page').addClass('mobile');
+    $('#bg-block').prepend('<div class="booking"><div><a href="#" class="btn">Book now</a></div></div>').prepend('<div class="bg-overlay"></div>');
 
-    //$('#contact-form-block').insertBefore('#contact');
+    $mobile.lngSwitcher();
+
     $('#contact').prepend($('#contact-form-block h2'));
 
     var homeAbout = $('#homepage-about'),
@@ -125,51 +67,13 @@ $mobile.init = function() {
     homeAbout.prepend(homeAbout.find('h2'));
     homeOffers.find('.title').after(homeOffers.find('.img-section'));
 
-
     setTimeout(function() {
         if ($main.map) {
             $main.map.setOptions({draggable: false});
         }
     }, 1000);
-
 };
 
-/*$mobile.reset = function() {
-    $('#page').removeClass('mobile');
-};*/
-
-/*$mobile.resizing = function(width) {
-    if (width < 705) {
-        if (!$mobile.mobileMode) {
-            $mobile.init();
-            $mobile.mobileMode = true;
-        }
-    } else if ($mobile.mobileMode) {
-        $mobile.reset();
-        $mobile.mobileMode = false;
-    }
-};*/
-
 $(document).ready(function() {
-
     $mobile.init();
-
-    /*$mobile.resizing(window.innerWidth || screen.availWidth);
-    $(window).resize(function() {
-        $mobile.resizing(this.innerWidth || screen.availWidth);
-    });*/
-
-    /*$mobile.initNav();
-    $mobile.initFooter();
-    var categoriesBox = $('#categories');
-    if (categoriesBox.length > 0) {
-        $mobile.initCategories(categoriesBox);
-    }
-    if ($main.slider) {
-        $mobile.initSlider();
-    }
-    if ($main.map) {
-        $main.map.setOptions({draggable: false});
-    }
-    $mobile.initPartner();*/
 });
